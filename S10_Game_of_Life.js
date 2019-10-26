acd4c58d34b32820f3e081c4b09464cca311e958
JinHao-L <JinHao-L@users.noreply.github.com>
2019-10-26 20:47:22 +0800

    Update S10_Game_of_Life.js
    - Clean up code syntax
    - Fixed error where alive characters are not brought to next gen

--- S10_Game_of_Life.js
+++ S10_Game_of_Life.js
@@ -1,29 +1,30 @@
 //Game of Life
 function make_game(rows, cols) {
     const game = [];
-    
-    function helper(x, y){
+
+    function helper(x, y) {
         if (y === 0) {
             if (x > 0) {
-                game[x-1] = [];
-                helper(x-1, cols);
-            } else { }
+                game[x - 1] = [];
+                helper(x - 1, cols);
+            } else {}
         } else {
-            game[x][y-1] = 0;
-            helper(x, y-1);
+            game[x][y - 1] = 0;
+            helper(x, y - 1);
         }
     }
-    
+
     helper(rows, 0);
     return game;
 }
 
 function num_of_live_neighbours(game, n, r, c) {
-    const r_chk = [((r-1+n) % n), r, ((r+1) % n)];
-    const c_chk = [((c-1+n) % n), c, ((c+1) % n)];
+
+    const r_chk = [((r - 1 + n) % n), r, ((r + 1) % n)];
+    const c_chk = [((c - 1 + n) % n), c, ((c + 1) % n)];
 
     let result = -1 * game[r][c];
-    
+
     for (let i = 0; i < 3; i = i + 1) {
         for (let j = 0; j < 3; j = j + 1) {
             result = result + game[r_chk[i]][c_chk[j]];
@@ -34,7 +35,7 @@ function num_of_live_neighbours(game, n, r, c) {
 
 function next_gen(game, n) {
     const next = make_game(n, n);
-    
+
     for (let i = 0; i < n; i = i + 1) {
         for (let j = 0; j < n; j = j + 1) {
             const x = num_of_live_neighbours(game, n, i, j);
@@ -42,34 +43,32 @@ function next_gen(game, n) {
             if (status === 0) {
                 if (x === 3) {
                     next[i][j] = 1;
-                }else {
-                    continue;
+                } else {
+                    next[i][j] = 0;
                 }
             } else {
-                if (x < 2) {
-                    next[i][j] = 0;
-                } else if (x > 3) {
-                    next[i][j] = 0;
+                if ((x === 2) && (x === 3)) {
+                    next[i][j] = 1;
                 } else {
-                    continue;
+                    next[i][j] = 0;
                 }
             }
         }
     }
-    
+
     return next;
 }
 
 function random(n, number_alive) {
-    if (number_alive > n*n) {
+    if (number_alive > n * n) {
         return "Error: Board is too small";
     } else {
         const game = make_game(n, n);
         let i = number_alive;
-        while(i > 0) {
-            const x = math_floor(math_random()*n);
-            const y = math_floor(math_random()*n);
-            
+        while (i > 0) {
+            const x = math_floor(math_random() * n);
+            const y = math_floor(math_random() * n);
+
             if (game[x][y] === 1) {
                 continue;
             } else {
@@ -87,7 +86,7 @@ function is_game_over(game) {
         let result = true;
         for (let i = 0; i < len; i = i + 1) {
             for (let j = 0; j < len; j = j + 1) {
-                if(game[i][j] === 1) {
+                if (game[i][j] === 1) {
                     result = false;
                     break;
                 } else {
@@ -97,27 +96,27 @@ function is_game_over(game) {
         }
         return result;
     } else {
-        
+
     }
 }
 
 let game = [];
 let gen_count = 0;
 const start = (n, number_alive) => {
-                            game = random(n, number_alive); 
-                            gen_count = 0;
-                            display("GENERATION: " + stringify(gen_count));
-                            return game;
-                        };
-const next = () =>  {
-                        gen_count = gen_count + 1;
-                        display("GENERATION: " + stringify(gen_count));
-                        game = next_gen(game, array_length(game)); 
-                        if (is_game_over(game)) {
-                            display(game);
-                            game = [];
-                            return "GAME OVER in GEN " + stringify(gen_count);
-                        } else {
-                            return game;
-                        }
-                    };
+    game = random(n, number_alive);
+    gen_count = 0;
+    display("GENERATION: " + stringify(gen_count));
+    return game;
+};
+const next = () => {
+    gen_count = gen_count + 1;
+    display("GENERATION: " + stringify(gen_count));
+    game = next_gen(game, array_length(game));
+    if (is_game_over(game)) {
+        display(game);
+        game = [];
+        return "GAME OVER in GEN " + stringify(gen_count);
+    } else {
+        return game;
+    }
+};
